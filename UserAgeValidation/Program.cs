419b151a09fa5837a3c7e4ad914799ef00628062
jjpuusaag <jjuripuusaag@gmail.com>
2024-02-21 11:13:50 +0200

    add week 2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Channels;
+
+Console.WriteLine("Please select your gender(m/f):");
+
+char userGender = Char.Parse(Console.ReadLine());
+Console.WriteLine("Please enter your  last name:");
+
+string userLastName = Console.ReadLine();
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome,{userLastName}!");
+}
+
